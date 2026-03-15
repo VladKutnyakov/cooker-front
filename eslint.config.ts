748f6aebfcf2262855ec38f5e191141b607eb358
vladkutnyakov <kutnyakov1@yandex.ru>
2026-03-15 23:49:29 +0400

    chore: add @stylistic/eslint-plugin and config
    - Install @stylistic/eslint-plugin as a dev dependency to enforce code style rules.
    - Apply the recommended stylistic configuration in eslint.config.ts.
    - Disable vue/multi-word-component-names rule to allow single-word component names.

--- eslint.config.ts
+++ eslint.config.ts
@@ -2,6 +2,7 @@ import { globalIgnores } from 'eslint/config'
 import { defineConfigWithVueTs, vueTsConfigs } from '@vue/eslint-config-typescript'
 import pluginVue from 'eslint-plugin-vue'
 import pluginOxlint from 'eslint-plugin-oxlint'
+import stylistic from '@stylistic/eslint-plugin'
 
 // To allow more languages other than `ts` in `.vue` files, uncomment the following lines:
 // import { configureVueProject } from '@vue/eslint-config-typescript'
@@ -9,6 +10,8 @@ import pluginOxlint from 'eslint-plugin-oxlint'
 // More info at https://github.com/vuejs/eslint-config-typescript/#advanced-setup
 
 export default defineConfigWithVueTs(
+  stylistic.configs.recommended,
+
   {
     name: 'app/files-to-lint',
     files: ['**/*.{vue,ts,mts,tsx}'],
@@ -18,6 +21,13 @@ export default defineConfigWithVueTs(
 
   ...pluginVue.configs['flat/essential'],
   vueTsConfigs.recommended,
+
+  {
+    name: 'app/rules',
+    rules: {
+      'vue/multi-word-component-names': 'off',
+    },
+  },
 
   ...pluginOxlint.buildFromOxlintConfigFile('.oxlintrc.json'),
 )
